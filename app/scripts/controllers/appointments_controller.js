5f0cf63577aeb4b74a2a13127eabd441f52ecf6f
Máté Marjai <mate.marjai@gmail.com>
2014-07-28 11:26:40 +0100

    Turn the visit type checkbox into a button group to be populated dynamically.

--- app/scripts/controllers/appointments_controller.js
+++ app/scripts/controllers/appointments_controller.js
@@ -1,6 +1,6 @@
 SmartClient.AppointmentsController = Ember.ArrayController.extend({
   // Helpers to get the filters
-  dates: function() { 
+  dates: function() {
     var self = this;
     var dates = this.get('all').mapBy('date').toArray().uniq();
     dates.unshiftObject('All');
@@ -11,20 +11,29 @@ SmartClient.AppointmentsController = Ember.ArrayController.extend({
     return datesList;
   }.property('model.date'),
 
-  service_providers: function() { 
+  service_providers: function() {
     return this.get('all').mapBy('service_provider_id').toArray().uniq();
   }.property('model.service_provider_id'),
 
-  categories: function() { 
+  categories: function() {
     return this.get('all').mapBy('appointment_category_id').toArray().uniq();
   }.property('model.appointment_category_id'),
+
+  visit_types: function(){
+    var self = this;
+    var visit_types = self.get('all').mapBy('visit_type').toArray().uniq()
+    return visit_types.map(function(vt) {
+      var selected = (vt == self.get('selectedVisitType'));
+      return Ember.Object.create({id: vt, name: vt.capitalize(), selected: selected});
+    });
+  }.property('model.visit_type'),
 
   all: function() {
     return this.get('store').all('appointment');
   }.property('model.@each'),
 
   // Filter toggles and trigger
-  postNatalOnly: false,
+  selectedVisitType: false,
   emergencyOnly: false,
   selectedDate: false,
   selectedSP: false,
@@ -32,11 +41,11 @@ SmartClient.AppointmentsController = Ember.ArrayController.extend({
 
   filterDidChange: function() {
     this.applyFilters();
-  }.observes('postNatalOnly', 'emergencyOnly', 'selectedDate', 'selectedSP', 'selectedCategory'),
+  }.observes('selectedVisitType', 'emergencyOnly', 'selectedDate', 'selectedSP', 'selectedCategory'),
 
   // Filter helpers
-  postNatalFilter: function(content) {
-    return this.filterHelper(content, 'visit_type', 'post-natal');
+  visitTypeFilter: function(content, visitType) {
+    return this.filterHelper(content, 'visit_type', visitType);
   },
 
   emergencyFilter: function(content) {
@@ -66,7 +75,7 @@ SmartClient.AppointmentsController = Ember.ArrayController.extend({
     var selectedDate = this.get('selectedDate');
     var selectedSP = this.get('selectedSP');
     var selectedCategory = this.get('selectedCategory');
-    var postNatal = this.get('postNatalOnly');
+    var selectedVisitType = this.get('selectedVisitType');
     var emergency = this.get('emergencyOnly');
     var appointments = this.get('all');
     this.set('content', appointments);
@@ -83,10 +92,10 @@ SmartClient.AppointmentsController = Ember.ArrayController.extend({
       appointments = this.categoryFilter(appointments, selectedCategory);
     }
 
-    if (postNatal) {
-      appointments = this.postNatalFilter(appointments);
+    if (selectedVisitType) {
+      appointments = this.visitTypeFilter(appointments, selectedVisitType);
     }
-  
+
     if (emergency) {
       appointments = this.emergencyFilter(appointments);
     }
@@ -94,7 +103,7 @@ SmartClient.AppointmentsController = Ember.ArrayController.extend({
     this.set('content', appointments);
     return this.get('content');
   },
-  
+
   //Actions
   actions: {
     filterByDate: function(date) {
@@ -105,12 +114,16 @@ SmartClient.AppointmentsController = Ember.ArrayController.extend({
       }
     },
 
+    filterByVisitType: function(type) {
+      this.set('selectedVisitType', type);
+    },
+
     clearFilters: function() {
       //TODO FIXME how to change the checkbox state from here? or from the view?
       this.set('selectedDate', false);
       this.set('selectedSP', false);
       this.set('selectedCategory', false);
-      this.set('postNatal', false);
+      this.set('selectedVisitType', false);
       this.set('emergency', false);
       this.applyFilters();
     }
