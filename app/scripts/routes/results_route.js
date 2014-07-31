58e70443366f85f04b2f475531a042da86b01614
John Smyth <smythj6@gmail.com>
2014-07-31 17:26:27 +0100

    Rearchitected the search and got DOB working as well

--- app/scripts/routes/results_route.js
+++ app/scripts/routes/results_route.js
@@ -1,18 +1,28 @@
 SmartClient.ResultsRoute = Ember.Route.extend({
   model: function (params) {
-    model = this.store.find('service_user', params.keyword)
+    var query_type = 'name';
+
+    if (params.keyword.match(/[hH][0-9]+/)) {
+      query_type = 'hospital_number'
+    } else if (params.keyword.match(/[0-9]{4}\-[0-9]{2}\-[0-9]{2}/)) {
+      query_type = 'dob'
+    }
+
+    var options = {}
+    options[query_type] = params.keyword
+    model = this.store.find('service_user', options)
 
     model.then(function () {
       model.set('keyword', params.keyword)
     }, function () {
       log("error")
-      model.deleteRecord()
     });
 
     return model;
   },
 
-  setupController: function(controller, model){
-    controller.set('model', model);
+  setupController: function(controller, model) {
+    controller.set('model', model.get('content'));
+    controller.set('keyword', model.get('keyword'));
   }
 });
