d88e0dbb74c6b31c6c62acfc3ad72d29ea40c473
Máté Marjai <mate.marjai@gmail.com>
2014-08-12 19:33:32 +0100

    Better Edit interface.

--- app/scripts/controllers/appointment_edit_controller.js
+++ app/scripts/controllers/appointment_edit_controller.js
@@ -1,14 +1,19 @@
 SmartClient.AppointmentEditController = Ember.ObjectController.extend({
   needs: 'appointment',
   content: {},
+  visit_types: [
+    {id: 'ante-natal', name: 'Ante-Natal'},
+    {id: 'post-natal', name: 'Post-Natal'}
+  ],
+  priorities: [
+    {id: 'other', name: 'Other'},
+    {id: 'emergency', name: 'Emergency'}
+  ],
 
   actions: {
-    save: function(){
+    submit: function(){
       self = this
-      model = self.get('controllers.appointment.model')
-      this.get('buffer').forEach(function(attr){
-        model.set(attr.key, attr.value);
-      });
+      model = self.get('model')
 
       model.save().then(function () {
         self.transitionToRoute('appointment', model);
