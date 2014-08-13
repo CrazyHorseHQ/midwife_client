38e43a24d0805a8c9577a747eccc714e6e3b600a
Máté Marjai <mate.marjai@gmail.com>
2014-08-13 15:07:28 +0100

    Fix appointment edit.

--- app/scripts/adapters/appointment_serializer.js
+++ app/scripts/adapters/appointment_serializer.js
@@ -10,6 +10,7 @@ SmartClient.AppointmentSerializer = DS.ActiveModelSerializer.extend({
     json['service_provider_id'] = record.get('service_provider.id');
     // Delete
     delete json.visit_logs;
+    delete json.tag_ids;
 
     return json;
   },
--- app/scripts/controllers/appointment_edit_controller.js
+++ app/scripts/controllers/appointment_edit_controller.js
@@ -12,8 +12,21 @@ SmartClient.AppointmentEditController = Ember.ObjectController.extend({
 
   actions: {
     submit: function(){
-      self = this
-      model = self.get('model')
+      var self = this;
+
+      var model = this.get('model').setProperties({
+        date: self.get('date'),
+        time: self.get('time'),
+        priority: self.get('priority'),
+        visit_type: self.get('visit_type')
+      });
+      if (self.get('service_provider_id')) {
+        model.set('service_provider', self.get('store').getById('service_provider', self.get('service_provider_id')));
+      }
+      if (self.get('service_user_id')) {
+        model.set('service_user', self.get('store').getById('service_user', self.get('service_user_id')));
+      }
+
 
       model.save().then(function () {
         self.transitionToRoute('appointment', model);
