757990df0a8cece10d1ce6a372dd7a94d5c60066
John Smyth <smythj6@gmail.com>
2014-08-19 13:15:10 +0100

    Update the SU to work with baby, pregnancy relationship
    Started on displaying the fields in editable way and removed the edit
    route components

--- app/scripts/models/baby_model.js
+++ app/scripts/models/baby_model.js
@@ -1,3 +1,5 @@
 SmartClient.Baby = DS.Model.extend({
-  hospital_number: DS.attr()
+  hospital_number: DS.attr(),
+  pregnancy: DS.belongsTo('pregnancy'),
+  service_user: DS.belongsTo('serviceUser')
 });
