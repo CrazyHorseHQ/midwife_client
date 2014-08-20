699cca2676791442baf723a3817c0f7673d4a92b
John Smyth <smythj6@gmail.com>
2014-08-20 12:49:28 +0100

    Update the gestation period calculation
    If the baby is born then it will have a delivery date. In this case we
    statically calculate the gestation using the difference between EDD and
    baby delivery_date_time

--- app/scripts/models/baby_model.js
+++ app/scripts/models/baby_model.js
@@ -1,5 +1,6 @@
 SmartClient.Baby = DS.Model.extend({
   hospital_number: DS.attr(),
+  delivery_date_time: DS.attr(),
   pregnancy: DS.belongsTo('pregnancy'),
   service_user: DS.belongsTo('serviceUser')
 });
--- app/scripts/models/service_user_model.js
+++ app/scripts/models/service_user_model.js
@@ -8,20 +8,38 @@ SmartClient.ServiceUser = DS.Model.extend({
   gestation: function () {
     pregnancies_content = this.get('pregnancies.content')
     var edd = moment(pregnancies_content[0].get('estimated_delivery_date'))
+    var baby_ddt = this.get('babies.content')[0].get('delivery_date_time')
 
-    if (moment().isBefore(moment(edd), 'd')) {
-      var diff = Math.ceil(edd.diff(moment(), 'd', true))
-      diff = (280 - diff) // 280 is 4 weeks
+    if (baby_ddt == undefined || baby_ddt == "") {
+      if (moment().isBefore(moment(edd), 'd')) {
+        var diff = Math.ceil(edd.diff(moment(), 'd', true))
+        diff = (280 - diff) // 280 is 40 weeks
 
-      if (diff % 7 === 0) {
-        return Math.floor(diff / 7) + " weeks"
-      } else if (diff < 7) {
-        return (diff % 7) + " days"
+        if (diff % 7 === 0) {
+          return Math.floor(diff / 7) + " weeks"
+        } else if (diff < 7) {
+          return (diff % 7) + " days"
+        } else {
+          return Math.floor(diff / 7) + " weeks " + (diff % 7) + " days"
+        }
       } else {
-        return Math.floor(diff / 7) + " weeks " + (diff % 7) + " days"
+        return "T + " + Math.floor(moment().diff(edd, 'd', true))
       }
     } else {
-      return "T + " + Math.ceil(moment().diff(edd, 'd', true))
+      if (moment(baby_ddt).isBefore(moment(edd), 'd')) {
+        var diff = Math.ceil(edd.diff(moment(baby_ddt), 'd', true))
+        diff = (280 - diff) // 280 is 40 weeks
+
+        if (diff % 7 === 0) {
+          return Math.floor(diff / 7) + " weeks"
+        } else if (diff < 7) {
+          return (diff % 7) + " days"
+        } else {
+          return Math.floor(diff / 7) + " weeks " + (diff % 7) + " days"
+        }
+      } else {
+        return "T + " + Math.floor(moment(baby_ddt).diff(edd, 'd', true))
+      }
     }
   }.property('current_edd'),
 
