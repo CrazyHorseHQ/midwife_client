042ceb4fa18b6032aa5e2fc2bbbe0f095e718bd9
Máté Marjai <mate.marjai@gmail.com>
2014-09-17 15:48:00 +0100

    Using var interval.

--- app/scripts/controllers/clinic_controller.js
+++ app/scripts/controllers/clinic_controller.js
@@ -61,7 +61,7 @@ SmartClient.ClinicController = Ember.ObjectController.extend({
           moment(this.get('date') + "T" + model.get('opening_time')),
           moment(this.get('date') + "T" + model.get('closing_time'))
         );
-        interval = model.get('appointment_interval');
+        var interval = model.get('appointment_interval');
 
     range.by('minutes', function (mom) {
       if (mom.minute() % interval == 0 || mom.minute() == 0) {
