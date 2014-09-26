51c74d7c9c20d30701e904da0cd49b30896aacb4
Máté Marjai <mate.marjai@gmail.com>
2014-09-26 17:03:12 +0100

    Pre-select the values already stored.

--- app/scripts/components/editable_multi_select_component.js
+++ app/scripts/components/editable_multi_select_component.js
@@ -10,4 +10,14 @@ SmartClient.EditableMultiSelectComponent = SmartClient.EditableFieldComponent.ex
     }
     return this.get('emptyValue');
   }.property('selectedValues.@each.label'),
+
+  didInsertElement: function () {
+    // Setup the selected values based on the content and selectValue
+    var values = this.get('selectValue').split(', ');
+    var selectedObj = this.get('selectContent').filter(function(item, index, enumerable) {
+      return values.contains(item.value);
+    });
+    this.set('selectedValues', selectedObj);
+    this._super();
+  }
 });
