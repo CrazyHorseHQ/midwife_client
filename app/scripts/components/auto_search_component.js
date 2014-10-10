338ec310408aa2653b45a39833ec61c41511f651
John Smyth <smythj6@gmail.com>
2014-10-10 16:37:50 +0100

    Small updates to the auto-search
    Add tT to the regex for hospital number search
    Make the search fire with at least 2 chars present

--- app/scripts/components/auto_search_component.js
+++ app/scripts/components/auto_search_component.js
@@ -6,6 +6,7 @@ SmartClient.AutoSearchComponent = Ember.Component.extend({
 
   actions: {
     resultSelected: function (service_user) {
+      this.set('searchString', '')
       this.sendAction('resultSelected', service_user)
     }
   },
@@ -13,14 +14,14 @@ SmartClient.AutoSearchComponent = Ember.Component.extend({
   searchResults: function() {
     var searchString = this.get('searchString');
 
-    if (!searchString){ return; }
+    if (!searchString || (searchString && searchString.length < 2)){ return; }
 
     var options = {}
 
-    if (searchString.match(/[hH][0-9]+/)) {
-      options['hospital_number'] = searchString.match(/[hH][0-9]+/)[0]
+    if (searchString.match(/[hHtT][0-9]+/)) {
+      options['hospital_number'] = searchString.match(/[hHtT][0-9]+/)[0]
 
-      searchString = searchString.replace(/[hH][0-9]+/, "").trim()
+      searchString = searchString.replace(/[hHtT][0-9]+/, "").trim()
     }
 
     if (searchString.match(/@[0-9]{2}\-[0-9]{2}\-[0-9]{4}/)) {
