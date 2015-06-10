d41069905144bcd5fff2f4ae065c2844e97791fe
Máté Marjai <mate.marjai@gmail.com>
2015-06-10 21:38:57 +0100

    tests

--- app/tests/integration/index.js
+++ app/tests/integration/index.js
@@ -12,11 +12,10 @@ SmartClient.rootElement = '#ember-testing';
 // This will be a single module running all the tests
 module('SmartClient root tests', {
   setup: function () {
-           SmartClient.reset();
            window.fakeServer = new FakeServer();
          },
   teardown: function() {
-              SmartClient.reset();
               window.fakeServer.restore();
+              SmartClient.reset();
             }
 });
