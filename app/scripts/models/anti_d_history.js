7d9ee24921705ef963afd3c223ef68da47236ff1
John Smyth <smythj6@gmail.com>
2015-06-15 17:34:34 +0100

    Add service_provider name to anti_d_history table

--- app/scripts/models/anti_d_history.js
+++ app/scripts/models/anti_d_history.js
@@ -2,6 +2,7 @@ SmartClient.AntiDHistory = DS.Model.extend({
   anti_d: DS.attr(),
   created_at: DS.attr(),
   pregnancy: DS.belongsTo('pregnancy'),
+  service_provider: DS.belongsTo('serviceProvider'),
 
   formatted_time: function () {
     return moment(this.get('created_at')).format('HH:mm, DD/MM/YYYY')
